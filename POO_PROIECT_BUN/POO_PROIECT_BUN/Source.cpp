a37436fcedaab873e1e0e82ec7f70bfaf14bb6d5
abutu02 <butucaruandreea19@stud.ase.ro>
2020-12-29 23:32:37 +0200

    softdeadline

--- POO_PROIECT_BUN/POO_PROIECT_BUN/Source.cpp
+++ POO_PROIECT_BUN/POO_PROIECT_BUN/Source.cpp
@@ -57,6 +57,72 @@ public:
         delete[]kmParcursiPerAn;
     }
 
+    void setMarca(char* marca)
+    {
+        this->marca = new char[strlen(marca) + 1];
+        strcpy_s(this->marca, strlen(marca) + 1, marca);
+
+    }
+
+    void setModel(char model[20])
+    {
+        strcpy_s(this->model, strlen(model) + 1, model);
+    }
+
+    void setCuloare(string culoare)
+    {
+        this->culoare = culoare;
+    }
+
+    void setAn(int an)
+    {
+        this->an = an;
+    }
+
+    void setKmParcursiPerAn(int* kmParcursiPerAn)
+    {
+        this->kmParcursiPerAn = new int[2020 - an + 1];
+        for (int i = 0; i <= 2020 - an; i++)
+        {
+            this->kmParcursiPerAn[i] = kmParcursiPerAn[i];
+        }
+    }
+
+    void setId(int id)
+    {
+        this->id = id;
+    }
+
+    char* getMarca()
+    {
+        return marca;
+    }
+
+    char* getModel()
+    {
+        return model;
+    }
+
+    string getCuloare()
+    {
+        return culoare;
+    }
+
+    int getAn()
+    {
+        return an;
+    }
+
+    int* getKmParcursiPerAn()
+    {
+        return kmParcursiPerAn;
+    }
+
+    int getId()
+    {
+        return id;
+    }
+
     Autovehicul operator= (Autovehicul& A)
     {
         this->marca = new char[strlen(A.marca) + 1];
@@ -91,7 +157,17 @@ public:
 
     friend istream& operator>> (istream& is, Autovehicul& A)
     {
+        is >> A.id;
+        is >> A.marca;
+        is >> A.model;
+        is >> A.culoare;
+        is >> A.an;
+        for (int i = 0; i <= 2020 - A.an; i++)
+        {
+            is >> A.kmParcursiPerAn[i];
+        }
 
+        return is;
     }
 
     int operator[](int index)
@@ -164,18 +240,114 @@ public:
 
 class SUV :public Autovehicul
 {
-    const char* combustibil;
+    char* combustibil;
     int nrLocuri;
     int* kmSchimbUlei;
     int nrSchimburiUlei;
 
-};
+    SUV(): Autovehicul()
+    {
+        combustibil = nullptr;
+        nrLocuri = 0;
+        kmSchimbUlei = nullptr;
+        nrSchimburiUlei = 0;
+    }
 
-class Camion :public Autovehicul
-{
-    int gabarit;
-    char* serieSasiu;
-    int* vitezaMaximaAdmisa; // pozitia 0: viteza maxima in localitate, 1: in afara localitatii, 2: drumuri europene, 3: autostrada
+    SUV(const char* marca, const char model[20], string culoare, const char* combustibil, int nrLocuri):Autovehicul(marca, model, culoare)
+
+    {
+        this->combustibil = new char[strlen(combustibil) + 1];
+        strcpy_s(this->combustibil, strlen(combustibil) + 1, combustibil);
+        this->nrLocuri = nrLocuri;
+        kmSchimbUlei = nullptr;
+        nrSchimburiUlei = 0;
+    }
+
+    SUV(const char* marca, const char model[20], string culoare, const char* combustibil, int nrLocuri, int nrSchimburiUlei, int* kmSchimbUlei) :Autovehicul(marca, model, culoare)
+    {
+        this->combustibil = new char[strlen(combustibil) + 1];
+        strcpy_s(this->combustibil, strlen(combustibil) + 1, combustibil);
+        this->nrLocuri = nrLocuri;
+        this->nrSchimburiUlei = nrSchimburiUlei;
+        this->kmSchimbUlei = new int[nrSchimburiUlei];
+        for (int i = 0; i < nrSchimburiUlei; i++)
+        {
+            this->kmSchimbUlei[i] = kmSchimbUlei[i];
+        }
+    }
+
+    SUV(SUV& A) :Autovehicul(A.getMarca(), A.getModel(), A.getCuloare(), A.getAn(), A.getKmParcursiPerAn())
+    {
+        this->combustibil = new char[strlen(A.combustibil) + 1];
+        strcpy_s(this->combustibil, strlen(A.combustibil) + 1, A.combustibil);
+        this->nrLocuri = A.nrLocuri;
+        this->nrSchimburiUlei = A.nrSchimburiUlei;
+        this->kmSchimbUlei = new int[A.nrSchimburiUlei];
+        for (int i = 0; i < A.nrSchimburiUlei; i++)
+        {
+            this->kmSchimbUlei[i] = A.kmSchimbUlei[i];
+        }
+    }
+    SUV operator=(const SUV& A)
+    {
+        if (combustibil != nullptr)
+        {
+            delete[] combustibil;
+        }
+
+        if (A.kmSchimbUlei != nullptr && A.nrSchimburiUlei>0)
+        {
+            for (int i = 0; i < A.nrSchimburiUlei; i++)
+            {
+                this->kmSchimbUlei[i] = A.kmSchimbUlei[i];
+            }
+        }
+
+        return *this;
+    }
+
+    ~SUV()
+    {
+        if (combustibil != nullptr)
+        {
+            delete[] combustibil;
+        }
+        if (kmSchimbUlei != nullptr)
+        {
+            delete[] kmSchimbUlei;
+        }
+
+    }
+
+    void setCombustibil(char* combustibil)
+    {
+        this->combustibil = new char[strlen(combustibil) + 1];
+        strcpy_s(this->combustibil, strlen(combustibil) + 1, combustibil);
+    }
+
+    void setNrLocuri(int nrLocuri)
+    {
+        this->nrLocuri = nrLocuri;
+    }
+
+    void setNrSchimburiUlei(int nrSchimburiUlei)
+    {
+        this->nrSchimburiUlei = nrSchimburiUlei;
+    }
+
+    void setKmSchimbUlei()
+    {
+        this->kmSchimbUlei = new int[nrSchimburiUlei];
+        for (int i = 0; i < nrSchimburiUlei; i++)
+        {
+            this->kmSchimbUlei[i] = kmSchimbUlei[i];
+        }
+    }
+
+    char* getCombustibil()
+    {
+        return combustibil;
+    }
 };
 
 class Coupe :public Autovehicul
@@ -184,6 +356,126 @@ class Coupe :public Autovehicul
     char* taraDeOrigine;
     int* preturiFiltre;
     int nrFiltre;
+public:
+    Coupe() :Autovehicul()
+    {
+        nrPortiere = 0;
+        taraDeOrigine = nullptr;
+        preturiFiltre = nullptr;
+        nrFiltre = 0;
+
+    }
+
+    Coupe(const char* marca, const char model[20], string culoare, int nrPortiere, const char* taraDeOrigine) :Autovehicul(marca, model, culoare)
+    {
+        this->nrPortiere = nrPortiere;
+        this->taraDeOrigine = new char[strlen(taraDeOrigine) + 1];
+        strcpy_s(this->taraDeOrigine, strlen(taraDeOrigine) + 1, taraDeOrigine);
+        preturiFiltre = nullptr;
+        nrFiltre = 0;
+    }
+
+    Coupe(const char* marca, const char model[20], string culoare, int an, int* kmParcursiPerAn, int nrPortiere, const char* taraDeOrigine, int* preturiFiltre, int nrFiltre) :
+        Autovehicul(marca, model, culoare, an, kmParcursiPerAn)
+    {
+        this->nrPortiere = nrPortiere;
+        this->taraDeOrigine = new char[strlen(taraDeOrigine) + 1];
+        strcpy_s(this->taraDeOrigine, strlen(taraDeOrigine) + 1, taraDeOrigine);
+        this->nrFiltre = nrFiltre;
+        this->preturiFiltre = new int[nrFiltre];
+        for (int i = 0; i < nrFiltre; i++)
+        {
+            this->preturiFiltre[i] = preturiFiltre[i];
+        }
+
+    }
+
+    Coupe(Coupe& A) :Autovehicul(A.getMarca(), A.getModel(), A.getCuloare(), A.getAn(), A.getKmParcursiPerAn())
+    {
+        this->nrPortiere = A.nrPortiere;
+        this->taraDeOrigine = new char[strlen(A.taraDeOrigine) + 1];
+        strcpy_s(this->taraDeOrigine, strlen(A.taraDeOrigine) + 1, A.taraDeOrigine);
+        this->nrFiltre = A.nrFiltre;
+        this->preturiFiltre = new int[A.nrFiltre];
+        for (int i = 0; i < A.nrFiltre; i++)
+        {
+            this->preturiFiltre[i] = A.preturiFiltre[i];
+        }
+    }
+
+    ~Coupe()
+    {
+        delete[]taraDeOrigine;
+        delete[]preturiFiltre;
+    }
+
+    void setNrPortiere(int nrPortiere)
+    {
+        this->nrPortiere = nrPortiere;
+    }
+
+    void setTaraDeOrigine(char* taraDeOrigine)
+    {
+        this->taraDeOrigine = new char[strlen(taraDeOrigine) + 1];
+        strcpy_s(this->taraDeOrigine, strlen(taraDeOrigine) + 1, taraDeOrigine);
+    }
+
+    void setPreturiFiltre(int* preturiFiltre)
+    {
+        this->preturiFiltre = new int[nrFiltre];
+        for (int i = 0; i < nrFiltre; i++)
+        {
+            this->preturiFiltre[i] = preturiFiltre[i];
+        }
+    }
+
+    void setNrFiltre(int nrFiltre)
+    {
+        this->nrFiltre = nrFiltre;
+    }
+
+    Coupe operator= (Coupe& A)
+    {
+
+        this->nrPortiere = A.nrPortiere;
+        this->taraDeOrigine = new char[strlen(A.taraDeOrigine) + 1];
+        strcpy_s(this->taraDeOrigine, strlen(A.taraDeOrigine) + 1, A.taraDeOrigine);
+        this->nrFiltre = A.nrFiltre;
+        this->preturiFiltre = new int[A.nrFiltre];
+        for (int i = 0; i < A.nrFiltre; i++)
+        {
+            this->preturiFiltre[i] = A.preturiFiltre[i];
+        }
+
+        return *this;
+    }
+
+    friend ostream& operator<< (ostream& os, Coupe A)
+    {
+        os << A.nrPortiere;
+        os << A.taraDeOrigine;
+        os << A.nrFiltre;
+        for (int i = 0; i < A.nrFiltre; i++)
+        {
+            os << A.preturiFiltre[i];
+        }
+
+        return os;
+    }
+
+    friend istream& operator>> (istream& is, Coupe& A)
+    {
+        is >> A.nrPortiere;
+        is >> A.taraDeOrigine;
+        is >> A.nrFiltre;
+        for (int i = 0; i < A.nrFiltre; i++)
+        {
+            is >> A.preturiFiltre[i];
+        }
+
+        return is;
+
+    }
 
 };
 
@@ -215,9 +507,7 @@ int main()
     }
 
 }
-//getteri, setteri ptr cls Autovehicul
-//ptr celelalte 3 clase constructori, destructori, operator =, operator >>, <<, getteri, setteri
-//ptr parcAuto: constructori, destructori, operator =, operator >>, <<, getteri, setteri
-//instalare github din linia de comanda
-//
+
+
+
 
